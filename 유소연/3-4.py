3872e41a38ea0bcbd2bb81732158920779f6ef44
Soyuen-Yu <101792456+Soyuen-Yu@users.noreply.github.com>
2022-10-19 11:11:37 +0900

    3-4

---
+++ 유소연/3-4.py
@@ -0,0 +1,15 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+count = 0
+while True :
+    if n == 1 :
+        break
+    if n%k == 0 :
+        n /= k
+        count+=1
+    else :
+        n -= 1
+        count+=1    
+         
+print(count)
