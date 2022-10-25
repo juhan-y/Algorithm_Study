cabb0291deb1e87e16690942adc5a8435b6420c5
minji <3dlalswl3@naver.com>
2022-10-25 20:47:53 +0900

    1번문제 품

--- 이민지/[1주차]그리디/1번문제.py
+++ 이민지/[1주차]그리디/1번문제.py
@@ -1,11 +1,11 @@
 n = int(input())
 sum = 0
-sum += n/500
+sum += n//500
 n = n%500
-sum += n/100
+sum += n//100
 n = n%100
-sum += n/50
+sum += n//50
 n = n%50
-sum += n/10
+sum += n//10
 n = n%10
 print(sum)
