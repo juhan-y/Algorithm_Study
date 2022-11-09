ca99a03b68c55a8c478120930474315f4e6df53c
kse <kseun11@hanmail.net>
2022-11-09 23:52:27 +0900

    [3주차]DFS, BFS 풀이

---
+++ 김세은/[3주차]DFS_BFS/11번문제.py
@@ -0,0 +1,34 @@
+#11번문제
+from collections import deque
+
+N, M = map(int, input().split())
+graph = []
+for i in range(N):
+    a = list(map(int, input()))
+    graph.append(a)
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x,y))
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx<0 or ny<0 or nx>=N or ny>=M:
+                continue
+
+            if graph[nx][ny] == 0:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return graph[N-1][M-1]
+
+print(bfs(0,0))
