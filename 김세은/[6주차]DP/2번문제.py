88f9d1a5d4ca938e8c21a1e65bb0f33f0deea561
kse <kseun11@hanmail.net>
2022-12-08 21:26:57 +0900

    [6주차]DP 풀이

---
+++ 김세은/[6주차]DP/2번문제.py
@@ -0,0 +1,12 @@
+# 2번문제
+d = [0]*100
+
+def fibo(x):
+    if x==1 or x==2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = fibo(x-1) + fibo(x-2)
+    return d[x]
+
+print(fibo(99))
