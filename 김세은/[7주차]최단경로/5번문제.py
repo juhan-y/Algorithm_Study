78722d63c142d113bedb5dc8b36d5bc99e740434
kse <kseun11@hanmail.net>
2022-12-22 09:01:57 +0900

    [7주차]최단경로 풀이

---
+++ 김세은/[7주차]최단경로/5번문제.py
@@ -0,0 +1,48 @@
+# 5번문제
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m, start = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+distance = [INF]*(n+1)
+
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+def dijkstra(start):
+    q = []
+    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여 큐에 삽입
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        # 가장 최단 거리가 짧은 노드에 대한 정보를 꺼내기
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+# 도달할 수 있는 노드의 개수
+cnt = 0
+# 도달할 수 있는 노드 중에서 가장 멀리 있는 노드와의 최단 거리
+max_distance = 0
+for d in distance:
+    if d != INF:
+        cnt += 1
+        max_distance = max(max_distance, d)
+
+# 시작 노드는 제외해야 하므로 cnt - 1을 출력
+print(cnt-1, max_distance)
+
+            
