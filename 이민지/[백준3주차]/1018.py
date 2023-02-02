1c4d1d575ac4691aeeb3b731d85366dbed41dcf8
minji <3dlalswl3@naver.com>
2023-02-02 19:24:26 +0900

    백준 풀이

---
+++ 이민지/[백준3주차]/1018.py
@@ -0,0 +1,15 @@
+n,m = map(int, input().split())
+pan = []
+cnt = []
+
+for _ in range(n):
+    pan.append(input().split())
+
+for x in range(n-7):
+    for y in range(m-7):
+        index1 = 0
+        index2 = 0
+        for i in range(x, x+8):
+            for j in range(y, y+8):
+                if 
+
